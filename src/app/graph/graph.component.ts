ec6da2a93cb8154f7abc7f66335cca07c1571682
kimo7520 <karim.saad2327@yahoo.com>
2024-11-11 23:52:02 +0200

    feat : graph

---
+++ src/app/graph/graph.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './graph.component.html',
+  styleUrl: './graph.component.scss'
+})
+export class GraphComponent {
+  @Input() data :any;
+}
