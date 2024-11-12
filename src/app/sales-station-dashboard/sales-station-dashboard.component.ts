ff978f102d2ef3f87510fb7b8240d8cfc057f179
kimo7520 <karim.saad2327@yahoo.com>
2024-11-12 21:53:02 +0200

    fix : error of'NM is not a number' for pipe '_DecimalPipe'

--- src/app/sales-station-dashboard/sales-station-dashboard.component.ts
+++ src/app/sales-station-dashboard/sales-station-dashboard.component.ts
@@ -8,15 +8,15 @@ Chart.register(...registerables);
 @Component({
   selector: 'app-sales-station-dashboard',
   standalone: true,
-  imports: [CommonModule,MatIconModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './sales-station-dashboard.component.html',
-  styleUrls: ['./sales-station-dashboard.component.scss']
+  styleUrls: ['./sales-station-dashboard.component.scss'],
 })
 export class SalesStationDashboardComponent {
   stations = [
     { name: 'Station Name', sales: 62721, progress: 80 },
     { name: 'Station Name', sales: 62721, progress: 80 },
-    { name: 'Station Name', sales: 'NM', progress: 50 }
+    { name: 'Station Name', sales: 'NM', progress: 50 },
   ];
 
   ngOnInit(): void {
@@ -27,22 +27,40 @@ export class SalesStationDashboardComponent {
     new Chart('radarChart', {
       type: 'radar',
       data: {
-        labels: ['00:00', '02:00', '04:00', '06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00'],
-        datasets: [{
-          data: [20, 30, 40, 50, 60, 70, 60, 50, 40, 30, 20, 10],
-          backgroundColor: 'rgba(30,144,255,0.2)',
-          borderColor: 'rgba(30,144,255,0.8)',
-          borderWidth: 1,
-        }]
+        labels: [
+          '00:00',
+          '02:00',
+          '04:00',
+          '06:00',
+          '08:00',
+          '10:00',
+          '12:00',
+          '14:00',
+          '16:00',
+          '18:00',
+          '20:00',
+          '22:00',
+        ],
+        datasets: [
+          {
+            data: [20, 30, 40, 50, 60, 70, 60, 50, 40, 30, 20, 10],
+            backgroundColor: 'rgba(30,144,255,0.2)',
+            borderColor: 'rgba(30,144,255,0.8)',
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
         scales: {
           r: {
             beginAtZero: true,
-            ticks: { display: false }
-          }
-        }
-      }
+            ticks: { display: false },
+          },
+        },
+      },
     });
+  }
+  isNumber(value: any): boolean {
+    return !isNaN(parseFloat(value)) && isFinite(value);
   }
 }
